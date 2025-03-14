e5839d9fd12b17a2d97f8678555ad62dcc83a0b3
ygm <ygm121351@163.com>
2025-03-14 08:00:56 +0800

    feat: add

---
+++ P4.1-five-cli/gmb-base-cli/bin/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// const lib = require('gmb-base-cli-lib')
+
+// console.log('sum是', lib.sum(1, 2))
+// console.log('minus是', lib.minus(24, 10))
+
+
+console.log('welcome base-cli --555')
